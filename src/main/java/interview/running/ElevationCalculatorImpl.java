5b230142937dec78c5d5882e40e8e8d27ea36cd6
Someone else <tstas92@gmail.com>
2024-07-21 02:51:52 +0300

    Rework (#1)
    * modified input data
    
    * removed solutions, tests
    
    * removed unnecessary modifier
    
    * formatting
    
    ---------
    
    Co-authored-by: Someone else <>

---
+++ src/main/java/interview/running/ElevationCalculatorImpl.java
@@ -0,0 +1,10 @@
+package interview.running;
+
+import java.util.Iterator;
+
+public class ElevationCalculatorImpl implements ElevationCalculator {
+    @Override
+    public int calculateElevation(Iterator<Integer> iterator) {
+        return 0;
+    }
+}
